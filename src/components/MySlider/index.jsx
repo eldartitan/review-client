5e27c463e4a2a49d19400331d2753654bc88cfc4
Eldar Amirgazin <tmshulk@gmail.com>
2023-01-09 18:56:00 +0500

    1.3.1 commit

--- src/components/MySlider/index.jsx
+++ src/components/MySlider/index.jsx
@@ -1,6 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./slick.css";
 import { useRef } from "react";
 import { Box } from "@mui/material";
 
